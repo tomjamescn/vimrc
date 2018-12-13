dbbaf2533d27b2a1eb8dd87c1965df3fe8e00fc7
tomjamescn <tomjamescn@qq.com>
2018-12-13 11:06:41 +0800

    添加备份

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#first, backup old config
+backup_dir=~/.vim_backup_`date +%Y-%m-%d_%H%M%S`
+mkdir $backup_dir
+mv ~/.vimrc $backup_dir/
+mv ~/.vim $backup_dir/
+mv ~/.viminfo $backup_dir/
+
 vim_version=`vim --version | head -n 1 | grep -oE 'VIM - Vi IMproved [0-9].' | grep -o '[0-9]'`
 
 echo "vim version:"
