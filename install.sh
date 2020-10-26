a0b88cea7af495cde93f4c16cc6c0dd593ffb070
tomjamescn <tomjamescn@gmail.com>
2020-10-26 19:32:48 +0800

    fix

--- install.sh
+++ install.sh
@@ -8,21 +8,21 @@ apt install -y vim vim-nox
 
 cat << EOF >> $HOME/.bashrc
 
-export VIMINIT='source $MYVIMRC'
 export MYVIMRC='~/.vim/vimrc'
+export VIMINIT='source $MYVIMRC'
 EOF
 
 cat << EOF >> $HOME/.zshrc
 
-export VIMINIT='source $MYVIMRC'
 export MYVIMRC='~/.vim/vimrc'
+export VIMINIT='source $MYVIMRC'
 EOF
 
 export VIMINIT='source $MYVIMRC'
 export MYVIMRC='~/.vim/vimrc'
 
 # install plug in vimrc
-$vim -c PlugInstall +qall
+vim -c PlugInstall +qall
 
 # Coc extensions
 vim -c 'CocInstall -sync coc-json coc-snippets coc-git coc-python|q'
