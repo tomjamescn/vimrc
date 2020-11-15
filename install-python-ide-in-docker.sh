e39dd0f46507c61e8b50cd40837729e2806f740f
tomjamescn <tomjamescn@gmail.com>
2020-11-15 21:27:56 +0800

    Update install-python-ide-in-docker.sh

--- install-python-ide-in-docker.sh
+++ install-python-ide-in-docker.sh
@@ -2,12 +2,16 @@ mkdir /etc/apt/sources.list.d/
 
 apt-get install -y curl git zsh build-essential software-properties-common
 
-
-
 sh -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/tools/install.sh)"
 
-
 curl -sL install-node.now.sh/lts | bash -s -- --yes
+
+add-apt-repository ppa:jonathonf/vim -y
+apt update
+apt install -y vim vim-nox
+
+export MYVIMRC='~/.vim/vimrc'
+export VIMINIT='source $MYVIMRC'
 
 git clone https://github.com/tomjamescn/vimrc.git $HOME/.vim 
 
@@ -17,15 +21,14 @@ export MYVIMRC='~/.vim/vimrc'
 export VIMINIT='source $MYVIMRC'
 
 EOF
-```
 
 
 
 cd $HOME/.vim \
     && git pull \
-    && git checkout only-python \
-    && sh $PWD/install.sh
+    && git checkout only-python
 
-echo "You may be run:\n\nvim -c 'CocInstall -sync coc-json coc-snippets coc-git coc-python|q'\n\n"
+vim -c PlugInstall +qall
 
 vim -c 'CocInstall -sync coc-json coc-snippets coc-git coc-python|q'
+
